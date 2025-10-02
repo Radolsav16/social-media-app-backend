cb8fd649408662dd9f13f20bdf80eedf7caaecb6
Radoslav Todorov <todorovradoslav18@gmail.com>
2025-10-02 07:40:45 +0300

    fix the problem with PORT and not running the app

---
+++ src/controllers/home.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const homeController = Router();
+
+
+// homeController.get('/home',(req,res)=>{
+//     res.send({
+//         "body":'home page!'
+//     });
+// })
+
+
+export default homeController;
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,49 @@
-import express from 'express'
 import dotenv from 'dotenv'
+import express,{Express,Request,Response} from 'express'
+import connectDatabase from './config/databaseConnection';
+// import initApp from './config/express';
 import cors from 'cors';
+// import router from './config/Route';
 dotenv.config()
 
-const app = express();
-app.use(express.json())
+const PORT = process.env.PORT || 3050;
+const DATABASE_URI = process.env.DATABASE_URI || '';
+
+const app:Express = express();
 app.use(cors());
-const PORT = process.env.PORT || 3030;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/',(req,res)=>{
-    res.send('Hello world!')
-})
-
-
-
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 
-app.listen(PORT,()=>console.log(`App is running on PORT ${PORT}`))
+app.get("/home", (req: Request, res: Response) => {
+    console.log(req)
+  res.send('Hello Home!')
+});
+
+
+
+async function start(){
+    try {
+    await connectDatabase(DATABASE_URI);
+    app.listen(PORT,()=>console.log(`App is running on PORT ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+   
+}
+start()
+
+
+app.listen(3050, () => {
+    console.log('Example app listening on port 3050!');
+    }
+);
+
+
+
+
+
